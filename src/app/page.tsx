35d58167fa4887f103922c7e251c96887b0cf5c1
Glaucoed <gesverzuthe@gmail.com>
2024-07-06 09:00:33 -0300

    feat: criado base do desafio

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+"use client"
+import { Container, Title } from "@/styles/styles"
+
+export default function Page() {
+  return (
+      <Container>
+        <Title>Buildbox - Web Challenge</Title>
+      </Container>
+  );
+}
